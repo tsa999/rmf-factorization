1188c5c7bb23addc0306a12afa183824b95ad07f
tsa999 <tsa999@ukr.net>
2025-07-19 13:35:22 +0300

    Update rmf_factor.py

--- rmf_factor.py
+++ rmf_factor.py
@@ -1,19 +1,21 @@
 import time
 import math
+
 def factor(n):
-    zp = 0
+    found = False
     y_max = math.isqrt(n) + 1
     t0 = time.time()
     for y in range(y_max, 1, -1):
-        z = n % y
-        if z == 0:
-             print(f"Знайдено дільник: {y}")
-             zp = 1
-        y -= 1
+        if n % y == 0:
+            print(f"Знайдено дільник: {y}, другий: {n // y}")
+            found = True
     t1 = time.time()
-    if zp == 0:
+    if not found:
         return f"Число просте. Час факторизації: {t1 - t0:.2f} сек"
+    else:
+        return f"Час факторизації: {t1 - t0:.2f} сек"
+
 # Тест
-n = 9361973132609
+n = 9361973132609  # 3 × 3 × 10402192484 × 10402192484
 result = factor(n)
-print("Result:",n, result)
+print("Result:", n, result)
